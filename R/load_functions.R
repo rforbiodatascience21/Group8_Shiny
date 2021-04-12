77086751abcf096f2d339f4d0269605616d39763
theis2 <patricktheisjensen@hotmail.com>
2021-04-12 10:41:18 +0200

    Added the last functions

---
+++ R/load_functions.R
@@ -0,0 +1,7 @@
+#Run the functions!
+
+source(file = "R/Random_DNA.R")
+source(file = "R/dna_codons_to_aa.R")
+source(file = "R/Translate_basesTo_AminoAcids.R")
+source(file = "R/aa_statistics.R")
+source(file = "R/peptide_weight.R")
