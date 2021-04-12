b4306b3d78953eba6e51d15f6c94ded23452e980
theis2 <patricktheisjensen@hotmail.com>
2021-04-12 11:37:31 +0200

    Fixed case sensitive letters

--- R/load_functions.R
+++ R/load_functions.R
@@ -1,6 +1,6 @@
 #Run the functions!
 
-source(file = "R/Random_DNA.R")
+source(file = "R/random_dna.R")
 source(file = "R/dna_codons_to_aa.R")
 source(file = "R/Translate_basesTo_AminoAcids.R")
 source(file = "R/aa_statistics.R")
